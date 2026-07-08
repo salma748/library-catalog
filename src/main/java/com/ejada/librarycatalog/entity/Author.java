f26a48161d9c1682b4642e1574abc5a76966568c
salma <salmamohamedhafez3@gmail.com>
2026-07-09 02:09:58 +0300

    Add Author entity

---
+++ src/main/java/com/ejada/librarycatalog/entity/Author.java
@@ -0,0 +1,61 @@
+package com.ejada.librarycatalog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String nationality;
+
+    public Author() {
+    }
+
+    public Author(String name, String email, String nationality) {
+        this.name = name;
+        this.email = email;
+        this.nationality = nationality;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}

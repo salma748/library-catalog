416763517318cc71444940b00fdcd8a66ec2d14a
salma <salmamohamedhafez3@gmail.com>
2026-07-09 04:16:32 +0300

    Add Book repository

---
+++ src/main/java/com/ejada/librarycatalog/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.ejada.librarycatalog.repository;
+
+import com.ejada.librarycatalog.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByAuthorId(Long authorId);
+}

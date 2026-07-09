4c661d0344b687f89ea610af15abdc0e72f5bc79
salma <salmamohamedhafez3@gmail.com>
2026-07-09 03:51:30 +0300

    Add Book entity

---
+++ src/main/java/com/ejada/librarycatalog/entity/Book.java
@@ -0,0 +1,80 @@
+package com.ejada.librarycatalog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    @Column(name = "publication_year")
+    private Integer publicationYear;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(
+            String title,
+            String isbn,
+            Integer publicationYear,
+            Author author) {
+
+        this.title = title;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
